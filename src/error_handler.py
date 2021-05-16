a347e30dac15a9b1fd26e2b3e9a11030dca9816f
wfh <ryanlattanzi@ryans-mbp.home>
2021-05-16 17:26:33 -0400

    fixing minor errors

--- src/error_handler.py
+++ src/error_handler.py
@@ -7,5 +7,5 @@ class WrongDFAmountError(Exception):
 
 class ServiceNotFoundError(Exception):
     def __init__(self, service_name):
-            self.message = 'Service {} is not available. Please choose either get_new_rows or dedupe_single_df.'
+            self.message = 'Service {} is not available. Please choose either get_new_rows or dedupe_single_df.'.format(service_name)
             super().__init__(self.message)
